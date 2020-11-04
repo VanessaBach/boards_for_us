5e520a399b56670db83e58577cb0984344ebc87a
Bernardo Neves <bernardosbneves@gmail.com>
2020-11-03 22:16:50 -0300

    changes for the dashboard

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["The board that fits to you"],
+    strings: ["The board that fits you"],
     typeSpeed: 100,
     loop: true
   });
