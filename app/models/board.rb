bb50330010761650c5698128f4bdf0d9fc2b449e
vanessabach <vanessabach.r@gmail.com>
2020-11-03 22:13:45 -0300

    done

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,5 +5,4 @@ class Board < ApplicationRecord
   has_one_attached :photo
   validates :style, :size, :year, :price_per_day, presence: true
   validates :photo, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
-
 end
