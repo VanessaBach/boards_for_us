5806ae697f1915e5c0c6a3fe02a0dfbccc7d9e07
Romulo Santos <romulosantos88@hotmail.com>
2020-11-05 20:35:29 -0300

    map is done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,13 +27,12 @@ import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { initMapbox } from '../plugins/init_mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  console.log('to aqui')
-
   initMapbox();
 	loadDynamicBannerText();
   initUpdateNavbarOnScroll();
