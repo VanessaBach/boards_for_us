5caba36ef67a4ccbe0dcc0bf925d96899fe90bd0
Romulo Santos <romulosantos88@hotmail.com>
2020-11-06 01:03:39 -0300

    autocomplete_v1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,7 +27,7 @@ import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { initMapbox } from '../plugins/init_mapbox';
-import { initAutocomplete } from '../plugins/init_place';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
