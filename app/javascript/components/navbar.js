e162acfe799d31fc2d5da92e70d4bb4aa25b8b2e
Bernardo Neves <bernardosbneves@gmail.com>
2020-11-05 21:18:49 -0300

    Navbar changed

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,19 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
+  const text = document.querySelectorAll('.nav-link');
+  console.log(text);
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 485) {
         navbar.classList.add('navbar-lewagon-white');
+        text.forEach((word)=> {
+          word.classList.add('nav-link-blue');
+        });
       } else {
         navbar.classList.remove('navbar-lewagon-white');
+        text.forEach((word)=> {
+          word.classList.remove('nav-link-blue');
+        });
       }
     });
   }
